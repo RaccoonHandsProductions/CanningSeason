880bc2d05f4964cbfaf61074a5ba4f86eba760ba
dottyhorsman <dottyhorsman@yahoo.com>
2022-03-14 10:00:53 -0400

    Add glow
    The default value of tappable was changed to false, which is
    appropriate for the game, but does make this scene a little
    harder to test in isolation.

--- project/PrepStation/Knife.gd
+++ project/PrepStation/Knife.gd
@@ -8,7 +8,7 @@ signal chopped
 export var debug_color := Color.whitesmoke
 
 # Indicates if the knife can be tapped to initiate the chop animation
-export var tappable := true
+export var tappable := false setget _set_tappable
 
 onready var _animation_player := $AnimationPlayer
 
@@ -16,6 +16,15 @@ func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	if tappable and not _animation_player.is_playing():
 		if event is InputEventMouseButton and event.is_pressed():
 			_animation_player.play("Chop")
+
+
+func _set_tappable(value:bool)->void:
+	tappable = value
+	if tappable:
+		_animation_player.play("Glow")
+	else:
+		_animation_player.play("RESET")
+		_animation_player.stop()
 
 
 func _emit_chopped_signal():
