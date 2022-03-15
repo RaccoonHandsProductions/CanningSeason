396248473e7133af09cb882fdef1f508f719a106
dottyhorsman <dottyhorsman@yahoo.com>
2022-03-15 16:11:28 -0400

    Carrot floats back when not above board

--- project/PrepStation/PrepStation.gd
+++ project/PrepStation/PrepStation.gd
@@ -2,13 +2,18 @@ extends Node2D
 
 # The duration of the animation between chop points
 export var knife_chop_transition_animation_duration := 0.3
-
 # The duration of the animation of knife from or to home
 export var knife_offscreen_animation_duration := .75
+
+
+export var carrot_float_animation_duration := 0.5
+
+
 
 enum _State {
 	AWAITING_CARROT_TOUCH,
 	DRAGGING_CARROT,
+	CARROT_FLOATING_HOME,
 	CHOPPING
 }
 
@@ -23,14 +28,31 @@ func _ready():
 func _input(event):
 	match _state:
 		_State.DRAGGING_CARROT:
-			if event is InputEventMouseMotion:
-				var motion_event := event as InputEventMouseMotion
-				$Carrot.position += event.relative
-			elif event is InputEventMouseButton and not event.is_pressed():
-				var above_board := Geometry.is_point_in_polygon($Carrot.position, $NewCuttingBoard.polygon)
-				if above_board:
-					_set_state(_State.CHOPPING)
+				if event is InputEventMouseMotion:
+					var motion_event := event as InputEventMouseMotion
+					$Carrot.position += event.relative
+				elif event is InputEventMouseButton and not event.is_pressed():
+					var above_board := Geometry.is_point_in_polygon($Carrot.position, $NewCuttingBoard.polygon)
+					if above_board:
+						_set_state(_State.CHOPPING)
+					else:
+						_set_state(_State.CARROT_FLOATING_HOME)
+						var tween := Tween.new()
+						add_child(tween)
+						tween.connect("tween_completed", self, "_set_state_to_awaiting_carrot_touch")
+						tween.interpolate_property(
+							$Carrot, "position", 
+							$Carrot.position, $CarrotSpawnPoint.position, 
+							carrot_float_animation_duration,
+						Tween.TRANS_QUAD, Tween.EASE_IN)
+						tween.start()
+		
 
+# Because this is bound to tween_completed, we have to have two arguments
+# that are ignored.
+func _set_state_to_awaiting_carrot_touch(_a, _b):
+	_set_state(_State.AWAITING_CARROT_TOUCH)
+	
 
 func _animate_Knife_to_next_chop_point(duration:float):
 	var tween := Tween.new()
@@ -93,6 +115,3 @@ func _on_Carrot_touched():
 			_set_state(_State.DRAGGING_CARROT)
 
 
-func _on_CuttingBoard_touch_released():
-	if _state == _State.DRAGGING_CARROT:
-		_set_state(_State.CHOPPING)
