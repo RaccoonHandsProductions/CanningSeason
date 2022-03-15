d36615d13321454310c835f1aff34234ec1e4016
dottyhorsman <dottyhorsman@yahoo.com>
2022-03-15 14:10:03 -0400

    Shorten knife animation time
    The two animation durations are controlled by the two new
    exported variables.

--- project/PrepStation/PrepStation.gd
+++ project/PrepStation/PrepStation.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
+# The duration of the animation between chop points
+export var knife_chop_transition_animation_duration := 0.3
 
-
+# The duration of the animation of knife from or to home
+export var knife_offscreen_animation_duration := .75
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_animate_Knife_to_next_chop_point()
+	_animate_Knife_to_next_chop_point(knife_offscreen_animation_duration)
 	
 
 
-func _animate_Knife_to_next_chop_point():
+func _animate_Knife_to_next_chop_point(duration:float):
 	var tween := Tween.new()
 	var next_pos :Vector2= $Carrot.position + $Carrot.current_chop_point_pos
 	add_child(tween)
@@ -18,7 +21,7 @@ func _animate_Knife_to_next_chop_point():
 # warning-ignore:return_value_discarded
 	tween.interpolate_property(
 		$Knife, "position", 
-		$Knife.position, next_pos, 1,
+		$Knife.position, next_pos, duration,
 		Tween.TRANS_QUAD, Tween.EASE_IN)
 # warning-ignore:return_value_discarded
 	tween.start()
@@ -38,7 +41,7 @@ func _on_Knife_chop_animation_complete():
 	$Knife.tappable = false
 	var next_pos = $Carrot.current_chop_point_pos
 	if next_pos!=null:
-		_animate_Knife_to_next_chop_point()
+		_animate_Knife_to_next_chop_point(knife_chop_transition_animation_duration)
 	else:
 		_animate_Knife_to_home()
 
@@ -51,7 +54,7 @@ func _animate_Knife_to_home():
 # warning-ignore:return_value_discarded
 	tween.interpolate_property(
 		$Knife, "position", 
-		$Knife.position, next_pos, 1,
+		$Knife.position, next_pos, knife_offscreen_animation_duration,
 		Tween.TRANS_QUAD, Tween.EASE_IN)
 # warning-ignore:return_value_discarded
 	tween.start()
