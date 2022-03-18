a11467a20d60c4e52c7484c98ce16b6a2bec0e9a
Jordan Demske <jmdemske@outlook.com>
2022-03-17 20:50:06 -0400

    Add basket and countertop, Update HUD for visibility (Hide testing messages)

--- project/PrepStation/HUD.gd
+++ project/PrepStation/HUD.gd
@@ -7,13 +7,13 @@ func _process(delta):
 # sets the Message label to a text value input for contexual HUD messages / testing states
 func show_message(text):
 	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
+	#$Message.show()		# Uncomment this to show state messages
+	#$MessageTimer.start()	# This removes state messages after a short period
 
 # sets the ScoreLabel based on a variable input
 func update_score(score):
 	$ScoreLabel.text = ( "Score: " + str(score) )
 
-
+# Hides message on MesageTimer timeout
 func _on_MessageTimer_timeout():
 	$Message.hide()
