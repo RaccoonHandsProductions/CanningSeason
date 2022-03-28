f2f968cb4c7c700da4d2de42c49762718819073f
dottyhorsman <dottyhorsman@yahoo.com>
2022-03-28 11:00:08 -0400

    burned it all

--- project/UI/IPEntryControl.gd
+++ project/UI/IPEntryControl.gd
@@ -7,18 +7,16 @@ var octet_2_entry : String
 var octet_3_entry : String
 
 var current_section
-var current_section_label
-var current_field_index := 1
+var current_field_index := 0
 var ip_octet : String
-var ip_array = []
 
-onready var field_one = $VBoxContainer/OctetDisplay/OctetField
-onready var field_two = $VBoxContainer/OctetDisplay/OctetField2
-onready var field_three = $VBoxContainer/OctetDisplay/OctetField3
-onready var field_four = $VBoxContainer/OctetDisplay/OctetField4
+onready var _fields = [$VBoxContainer/OctetDisplay/OctetField, 
+$VBoxContainer/OctetDisplay/OctetField2, 
+$VBoxContainer/OctetDisplay/OctetField3, 
+$VBoxContainer/OctetDisplay/OctetField4]
 
-onready var back_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/userBoxPosition/PreviousBoxPriority
-onready var next_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/userBoxPosition/NextBoxPriority
+onready var back_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/UserBoxPosition/PreviousBoxPriority
+onready var next_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/UserBoxPosition/NextBoxPriority
 onready var enter_button = $VBoxContainer/HBoxContainer2/VBoxContainer2/HBoxContainer/EnterButton
 onready var number_button_array = get_tree().get_nodes_in_group("number_buttons")
 
@@ -26,7 +24,8 @@ func _ready():
 	for button in number_button_array:
 		button.connect("pressed", self, "on_number_Button_pressed", 
 			[ int( button.name.substr(5) ) ])
-	_choose_display_field(current_field_index)
+	back_button.disabled = false
+
 
 func validate_octet(oct : String):
 	if (int(oct) >= 0 and int(oct) <= 255):
@@ -34,122 +33,11 @@ func validate_octet(oct : String):
 	else:
 		return false
 
+func toggle_number_buttons_disabled(disabled : bool):
+	for buttons in number_button_array:
+			buttons.disabled = disabled
+
 func finialize_address(octet_0 : String, octet_1 : String, 
 	octet_2 : String, octet_3 : String):
+			return(octet_0 +"."+ octet_1 +"."+ octet_2 +"."+ octet_3)
 
-		var octet_array = [int(octet_0), int(octet_1), int(octet_2), int(octet_3)]
-		for octet in octet_array:
-			if octet < 0 or octet > 255:
-				assert(false, "your IP does not exist")
-			else:
-				return(octet_0 +"."+ octet_1 +"."+ octet_2 +"."+ octet_3)
-
-func on_number_Button_pressed(num:int):
-	if ip_octet.length() <= 2:
-		ip_octet += str(num)
-		print(ip_octet)
-		current_section_label.text += str(num)
-	
-	if ip_octet.length() == 3:
-		if validate_octet(ip_octet) == true:
-			ip_array.append(ip_octet)
-			ip_octet = ""
-			for buttons in number_button_array:
-				buttons.disabled = true
-			if current_section == field_four:
-				enter_button.disabled = false
-			update()
-		else:
-			ip_octet = ""
-			current_section_label.text = ""
-			update()
-			_choose_display_field(current_field_index)
-	print(ip_array)
-
-func _choose_display_field(field):
-	match field:
-		1:
-			current_section = field_one
-			current_field_index = 1
-			current_section_label = field_one.get_node("Label")
-			current_section.has_focus = true
-			field_two.has_focus = false
-			field_three.has_focus = false
-			field_four.has_focus = false 
-			back_button.disabled = true
-			enter_button.disabled = true
-		2:
-			current_section = field_two
-			current_field_index = 2
-			current_section_label = field_two.get_node("Label")
-			field_one.has_focus = false
-			current_section.has_focus = true
-			field_three.has_focus = false
-			field_four.has_focus = false
-			back_button.disabled = false
-			next_button.disabled = false
-		3:
-			current_section = field_three
-			current_field_index = 3
-			current_section_label = field_three.get_node("Label")
-			field_one.has_focus = false
-			field_two.has_focus = false
-			current_section.has_focus = true
-			field_four.has_focus = false
-			back_button.disabled = false
-			next_button.disabled = false
-		4:
-			current_section = field_four
-			current_field_index = 4
-			current_section_label = field_four.get_node("Label")
-			field_one.has_focus = false
-			field_two.has_focus = false
-			field_three.has_focus = false
-			current_section.has_focus = true
-			back_button.disabled = false
-			next_button.disabled = true
-	update()
-
-func _on_NextBoxPriority_pressed():
-	for buttons in number_button_array:
-		buttons.disabled = false
-	ip_octet = ""
-	current_field_index += 1
-	_choose_display_field(current_field_index)
-
-func _on_PreviousBoxPriority_pressed():
-	ip_array.pop_back()
-	current_section_label.text = ""
-	current_field_index -= 1
-	_choose_display_field(current_field_index)
-	ip_array.pop_back()
-	current_section_label.text = ""
-	if number_button_array[2].disabled == true:
-		for buttons in number_button_array:
-				buttons.disabled = false
-	print(ip_array)
-
-func _on_ClearBox_pressed():
-	ip_array.clear()
-	field_one.get_node("Label").text = ""
-	field_two.get_node("Label").text = ""
-	field_three.get_node("Label").text = ""
-	field_four.get_node("Label").text = ""
-	ip_octet = ""
-	current_field_index = 1
-	
-	for buttons in number_button_array:
-		buttons.disabled = false
-	next_button.disabled = false
-	_choose_display_field(current_field_index)
-
-func _on_EnterButton_pressed():
-	ip_array.append(ip_octet)
-	ip_octet = ""
-	if ip_array.size() == 4 or (ip_array.size() == 5 and ip_array[4] == ""):
-		var output = finialize_address(ip_array.pop_front(), ip_array.pop_front(), ip_array.pop_front(), ip_array.pop_front())
-		print(output)
-		assert(false, "IP is complete: " + output)
-	else:
-		assert(false, "IP is not complete")
-	#close scene and use ip
