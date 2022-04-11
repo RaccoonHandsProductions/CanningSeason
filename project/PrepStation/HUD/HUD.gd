2bb257693438322bfb77f8092327790552d55c12
May <brmay@bsu.edu>
2022-04-11 10:48:57 -0400

    Fix the bug in the HUD that breaks when the carrot increments.

--- project/PrepStation/HUD/HUD.gd
+++ project/PrepStation/HUD/HUD.gd
@@ -2,11 +2,11 @@ extends CanvasLayer
 
 signal Times_Up
 
+var _carrot_count = 0
+var _clean_count = 0
+var _full_count = 0
 # Displays time left on timer as a string from an integer from the timer, in the TimeLabel
 func _process(_delta):
-	update_Carrot_count(0)
-	update_Clean_Jar_count(0)
-	update_Full_Jar_count(0)
 	$TimeLabel.set_text( "Time: " + str( int( $GameTimer.get_time_left() ) ) )
 
 # sets the Message label to a text value input for contexual HUD messages / testing states
@@ -16,29 +16,29 @@ func show_message(text):
 	#$MessageTimer.start()	# This removes state messages after a short period
 
 # sets the ScoreLabel based on a variable input
-var count = 0
-func update_Carrot_count(carrotPiece):
-	if (carrotPiece != 100):
-		count += carrotPiece
-	else:
-		count = 0;
-	$ScoreLabel.text = ( "Score: " + str(count) )
+# Carrot counter should signal the server 
+# and will update carrot_count with a +1 if carrot chop completed, -1 if jar completed.
+func increment_Carrot_count():
+	_carrot_count += 1
+	$ScoreLabel.text = ( "Score: " + str(_carrot_count) )
 	
-var cleanCount = 0
-func update_Clean_Jar_count(cleanJar):
-	if (cleanJar != 100):
-		cleanCount += cleanJar
-	else:
-		cleanCount = 0;
-	$CleanJarLabel.text = ( "Clean Jars: " + str(cleanCount) )
+# Sanitized Jar counter should signal 
+# the server and update Sanitized Jar Count by +1, -1 if jar completed.
+func increment_Clean_Jar_count():
+	_clean_count += 1
+	$CleanJarLabel.text = ( "Clean Jars: " + str(_clean_count) )
 	
-var fullCount = 0
-func update_Full_Jar_count(fullJar):
-	if (fullJar != 100):
-		fullCount += fullJar
-	else:
-		fullCount = 0;
-	$FullJarLabel.text = ( "Full Jars: " + str(fullCount) )
+func increment_Full_Jar_count():
+	_full_count += 1
+	$FullJarLabel.text = ( "Full Jars: " + str(_full_count) )
+
+func decrement_Carrot_count():
+	_carrot_count -= 1
+	$ScoreLabel.text = ( "Score: " + str(_carrot_count) )
+	
+func decrement_Clean_Jar_count():
+	_clean_count -= 1
+	$CleanJarLabel.text = ( "Clean Jars: " + str(_clean_count) )
 	
 # Hides message on MesageTimer timeout
 func _on_MessageTimer_timeout():
