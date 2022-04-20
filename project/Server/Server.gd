b4dcb20106023934103a4fbc56a760eedc5743f5
Summers <ljsummers@bsu.edu>
2022-04-20 10:15:52 -0400

    Fix fill station not loading on tablets

--- project/Server/Server.gd
+++ project/Server/Server.gd
@@ -1,29 +1,36 @@
 extends Node
 
 const DEFAULT_PORT := 3333
+
+const _PrepStation := preload("res://PrepStation/PrepStation.tscn")
+const _JarSanitizationStation := preload("res://JarSanitizationStation/JarSanitizationStation.tscn")
+const _FillingStation := preload("res://FillingStation/FillingStation.tscn")
+
 var CARROT_CHUNKS := 0
 var SANITIZED_JARS := 0
 var FILLED_JARS := 0
-var index := 1
+var index := 0
 
 var stations = [
-	"_load_prep_station" , 
+	"_load_prep_station",
 	"_load_jar_sanitization_station", 
-	"_load_filling_station"
-	]
+	"_load_filling_station",
+]
 
 remote func start_game() -> void:
 	assert(get_tree().is_network_server(), "Should only be called on server.")
-	_load_prep_station()
+	var number_of_stations : int = stations.size()
+	
 	for peer_id in get_tree().get_network_connected_peers():
-		rpc_id(peer_id, stations[index%3])
+		rpc_id(peer_id, stations[index % number_of_stations])
 		index = index + 1
-
+		
+	call(stations[index % number_of_stations])
 
 
 remote func _load_prep_station() -> void:
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://PrepStation/PrepStation.tscn")
+	get_tree().change_scene_to(_PrepStation)
 
 
 remote func _update_carrot_chunk_count() -> void:
@@ -41,10 +48,6 @@ remote func _request_add_carrot_chunk() -> void:
 	rpc_id(1, "_update_carrot_chunk_count")
 
 
-
-
-
-
 remote func _start_jar_sanitization_station() -> void:
 	assert(get_tree().is_network_server(), "Should only be called on server.")
 	for peer_id in get_tree().get_network_connected_peers():
@@ -54,7 +57,7 @@ remote func _start_jar_sanitization_station() -> void:
 
 remote func _load_jar_sanitization_station() -> void:
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://JarSanitizationStation/JarSanitizationStation.tscn")
+	get_tree().change_scene_to(_JarSanitizationStation)
 
 
 remote func _update_sanitized_jar_count() -> void:
@@ -72,12 +75,6 @@ remote func _request_add_sanitized_jar() -> void:
 	rpc_id(1, "_update_sanitized_jar_count")
 
 
-
-
-
-
-
-
 remote func _start_filling_station() -> void:
 	assert(get_tree().is_network_server(), "Should only be called on server.")
 	for peer_id in get_tree().get_network_connected_peers():
@@ -87,7 +84,7 @@ remote func _start_filling_station() -> void:
 
 remote func _load_filling_station() -> void:
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://FillingStation/FillingStation.tscn")
+	get_tree().change_scene_to(_FillingStation)
 
 
 remote func _update_filled_jar_count() -> void:
