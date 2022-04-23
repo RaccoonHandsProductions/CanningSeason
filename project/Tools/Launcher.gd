d1bc1d28b4be6a8efa80e68d483cb5a6552e98d1
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-04-23 12:35:46 -0400

    Add launcher tool
    It launches N instances of the game locally. The process
    identifiers (pids) are kept so that it can easily kill
    them as well.

---
+++ project/Tools/Launcher.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var _pids := []
+
+onready var _spinbox := find_node("SpinBox")
+onready var _kill_button := find_node("KillButton")
+
+
+func _on_LaunchButton_pressed():
+	var number := _spinbox.value as int
+	assert(number > 0)
+	for i in number:
+		var pid = OS.execute(OS.get_executable_path(), [], false)
+		_pids.append(pid)
+	_kill_button.disabled = false
+
+
+func _on_KillButton_pressed():
+	while not _pids.empty():
+		var pid : int = _pids.pop_front()
+		var error := OS.kill(pid)
+		if error:
+			print("Error encountered killing process %d: %s" % [pid, str(error)])
+	_kill_button.disabled = true
